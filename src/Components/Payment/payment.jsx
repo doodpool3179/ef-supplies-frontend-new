afc65c43c44ec3cdac7977a132b3498cf030bd30
nickjmo99 <nicholas.moloney@yahoo.com>
2024-12-18 05:15:04 -0500

    home page
    stayed up until 5 AM

--- src/Components/Payment/payment.jsx
+++ src/Components/Payment/payment.jsx
@@ -7,13 +7,13 @@ import Nav from '../Navbar/Navbar.jsx'
 const Payment = () => {
     return(
         
-        <body>
+        <div>
             <link rel="stylesheet" href="Payment.css"></link>
             <Nav></Nav>
-            <div class ="container">
+            <div class ="pcontainer">
                 <form action="">
-                    <div class="row">
-                        <div class="column">
+                    <div class="prow">
+                        <div class="pcolumn">
                             <h3 class ="title">Billing Address</h3>
                             <div class="input-box">
                                 <span>Full Name: </span>
@@ -21,33 +21,33 @@ const Payment = () => {
                                    placeholder="John Smith">
                                 </input>    
                             </div>
-                            <div class="input-box">
+                            <div class="pinput-box">
                                 <span>Email :</span>
                                 <input type="email"
                                 placeholder="johnsmith@example.com">
                                 </input>
                             </div>
-                            <div class="input-box">
+                            <div class="pinput-box">
                                 <span>Address :</span>
                                 <input type="text"
                                 placeholder="Room  -  Street  -  Locality">
                                 </input>
                             </div>
-                            <div class="input-box">
+                            <div class="pinput-box">
                                 <span>City :</span>
                                 <input type="text"
                                 placeholder="Valhalla">
                                 </input>
                             </div>
 
-                            <div class="flex">
-                                <div class="input-box">
+                            <div class="pflex">
+                                <div class="pinput-box">
                                     <span>State :</span>
                                     <input type="text"
                                     placeholder="New York">
                                     </input>    
                                 </div>
-                                <div class="input-box">
+                                <div class="pinput-box">
                                     <span>Zip Code :</span>
                                     <input type="number"
                                     placeholder="00000">
@@ -56,46 +56,46 @@ const Payment = () => {
                             </div>
                         </div>
 
-                        <div class="column">
+                        <div class="pcolumn">
                             <h3 class ="title">Payment</h3>
                             <div class="input-box">
                                 <span>Methods Accepted: </span>
                                 <img src={cards} alt="Cards"></img>
                            
                             </div>
-                            <div class="input-box">
+                            <div class="pinput-box">
                                 <span>Name On Card :</span>
                                 <input type="text"
                                 placeholder="John Smith">
                                 </input>
                             </div>
-                            <div class="input-box">
+                            <div class="pinput-box">
                                 <span>Credit Card Number :</span>
                                 <input type="number"
                                 placeholder="0000 2222 4444 7777">
                                 </input>
                             </div>
-                            <div class="input-box">
+                            <div class="pinput-box">
                                 <span>Expiration Date :</span>
                                 <input type="text"
                                 placeholder="MM/YYYY">
                                 </input>
                             </div>
-                            <div class="input-box">
+                            <div class="pinput-box">
                                 <span>CVV :</span>
                                 <input type="number"
                                 placeholder="777">
                                 </input>
                             </div>
 
-                            <div class="flex">
-                                <div class="input-box">
+                            <div class="pflex">
+                                <div class="pinput-box">
                                     <span>State :</span>
                                     <input type="text"
                                     placeholder="New York">
                                     </input>    
                                 </div>
-                                <div class="input-box">
+                                <div class="pinput-box">
                                     <span>Zip Code :</span>
                                     <input type="number"
                                     placeholder="00000">
@@ -108,7 +108,7 @@ const Payment = () => {
                     <button type="submit" class="btn">Submit</button>
                 </form>
             </div>
-        </body>
+        </div>
     );
 };
 
